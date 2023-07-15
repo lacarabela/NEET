c846d3143727d5460f9efa65adaa6a6cf9c8cb1e
lacarabela <maldonadomunoz@gmail.com>
2023-07-15 00:00:17 -0400

    Update

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def connect_to_database():
     conn = psycopg2.connect(database="manga_database",
                             host="localhost",
                             user="postgres",
-                            password="Mamita0011!!")
+                            password="postgres")
 
     cursor = conn.cursor()
     return conn, cursor
